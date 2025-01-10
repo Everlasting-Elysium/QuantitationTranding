bb4fb2717684b743f31b01815ba0751d3a7de3e9
yun.wu4 <yun.wu4@nio.com>
2025-01-10 18:11:32 +0800

    【feature】project init

---
+++ server.js
@@ -0,0 +1,15 @@
+const { createServer } = require('node:http');
+
+const host = "127.0.0.1";
+
+const port = 3000;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Elysia\n');
+    });
+
+const listen = server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+    });
